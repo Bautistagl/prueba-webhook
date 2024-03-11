56f56ec1b8c6e40c0fb03940f7bc0094a43ff1d2
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-03-11 15:15:41 -0300

    dsadsa

--- index.js
+++ index.js
@@ -17,47 +17,69 @@
 // app.listen(3000, () => {
 //     console.log('Server is listening on 3000')
 // })
-import { App, createNodeMiddleware } from "octokit";
-import "dotenv/config";
-import express from "express";
+// import { App, createNodeMiddleware } from "octokit";
+// import "dotenv/config";
+// import express from "express";
 
 
 
 
-const ghApp = new App({
-  appId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY,
-  webhooks: {
-    secret: process.env.WEBHOOK_SECRET,
-  },
-  oauth: { clientId: null, clientSecret: null },
+// const ghApp = new App({
+//   appId: process.env.APP_ID,
+//   privateKey: process.env.PRIVATE_KEY,
+//   webhooks: {
+//     secret: process.env.WEBHOOK_SECRET,
+//   },
+//   oauth: { clientId: null, clientSecret: null },
+// });
+// const app = express();
+
+
+// app.get('/', (req,res)=>{
+//     res.send('Hello World!')
+// })
+
+
+// app.post('/api/github/webhooks', (req,res)=>{
+//   res.send('INSTALACION')
+//   console.log(res,'esto es res')
+//   console.log(req,'esto es req')
+// })
+// app.get('/api/github/webhooks', (req,res)=>{
+//   res.send('INSTALACION')
+//   console.log(res,'esto es res')
+//   console.log(req,'esto es req')
+// })
+// ghApp.webhooks.on('installation.created', async ({ octokit, payload }) => {
+//   console.log(`Received a installation.created event for #${payload.installation.id}`)
+// })
+
+
+
+
+// app.use(createNodeMiddleware(ghApp));
+// app.listen(3000, () => {
+//     console.log('Server is listeningg on 3000')
+// })
+const http = require('http');
+
+// Definir la función de manejo de solicitudes
+const requestHandler = (request, response) => {
+  // Establecer el encabezado de respuesta con el tipo de contenido
+  response.setHeader('Content-Type', 'text/plain');
+  // Escribir la respuesta
+  response.end('¡Hola, mundo!\n');
+};
+
+// Crear un servidor HTTP y pasar la función de manejo de solicitudes
+const server = http.createServer(requestHandler);
+
+// Escuchar en el puerto 3000 y la dirección localhost
+server.listen(3000, 'localhost', (err) => {
+  // Manejar los errores si los hay
+  if (err) {
+    return console.error('Error al iniciar el servidor:', err);
+  }
+  // Indicar que el servidor está funcionando
+  console.log('Servidor corriendo en http://localhost:3000/');
 });
-const app = express();
-
-
-app.get('/', (req,res)=>{
-    res.send('Hello World!')
-})
-
-
-app.post('/api/github/webhooks', (req,res)=>{
-  res.send('INSTALACION')
-  console.log(res,'esto es res')
-  console.log(req,'esto es req')
-})
-app.get('/api/github/webhooks', (req,res)=>{
-  res.send('INSTALACION')
-  console.log(res,'esto es res')
-  console.log(req,'esto es req')
-})
-ghApp.webhooks.on('installation.created', async ({ octokit, payload }) => {
-  console.log(`Received a installation.created event for #${payload.installation.id}`)
-})
-
-
-
-
-app.use(createNodeMiddleware(ghApp));
-app.listen(3000, () => {
-    console.log('Server is listeningg on 3000')
-})
